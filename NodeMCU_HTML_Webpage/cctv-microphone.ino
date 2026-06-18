1c8ed64b9bcaa3cf3efd91f4370a2344ded83568
Julfikar24 <121005671+Julfikar24@users.noreply.github.com>
2026-06-18 18:40:28 +0530

    Update cctv-microphone.ino

--- NodeMCU_HTML_Webpage/cctv-microphone.ino
+++ NodeMCU_HTML_Webpage/cctv-microphone.ino
@@ -1,4 +1,18 @@
+-----------esp32-------------------
 #define MIC_PIN 34
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int value = analogRead(MIC_PIN);
+  Serial.println(value);
+  delay(10);
+}
+
+----------------------esp8266-------------------
+#define MIC_PIN A0
 
 void setup() {
   Serial.begin(115200);
